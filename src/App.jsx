13c657848ef44eb0df5df6a1fb5ee76222e1cabb
liviaamota <livia.mota11@aluno.ce.gov.br>
2025-04-22 08:54:32 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+
+export default function App() {
+  return(<div>
+    <img src="react.png" alt="" />
+    <h1> React </h1>
+    <p>A biblioteca para interfaces de usuario web e nativas.</p>
+     <button>Aprenda React</button> <button>Referencia da API</button> <hr />
+     <h2>Crie interfaces de usuario de componentes </h2>
+     <p>React permite que você construa interfaces de usuário a partir e peaços indiviuais chamados componentes.</p> <hr />
+      <h2> Escreva componente com código e marcação </h2>
+      <p>Componentes React são funçõe JavaScript. A sintaxe de marcação é chamaa de JSX. É uma extensão de sintaxe JavaScipt popularizada pelo React.</p> <hr />
+      <h2>Próximos passos </h2> 
+      <ul>
+        <li>
+          Uso de dados dinâmicos no JSX.
+        </li>
+        <li>
+        Criação de novos componentes.
+        </li>
+        <li>
+          Estilização de componentes.
+        </li>
+        <li>
+          Reutilizção de componentes.
+        </li>
+        <li>
+          Uso de props e children.
+        </li>
+        <li>
+          Uso de eventos do JavaScript.
+        </li>
+      </ul>
+  </div>)
+  
+}
